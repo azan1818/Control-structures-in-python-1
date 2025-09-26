72f10fc9377db3c2a6357c2e55a923830903db72
azan1818 <azan18181818@gmail.com>
2025-09-26 14:49:35 +0530

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,10 @@
+#Task1 : check if a number is even or odd
+
+#Take integer input from user
+num = int(input("Enter a number: "))
+
+#Check if number is even or odd
+if num % 2 == 0:
+    print(num,"is an a Even number.")
+else:
+     print(num,"is an a Odd number.")
